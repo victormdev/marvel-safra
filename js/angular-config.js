bcb8bbe270c75f0f9a469f2993c85e672552b381
victormdev <victoralexandrino99@gmail.com>
2021-12-22 03:14:45 -0300

    create constant definitions and api url

--- js/angular-config.js
+++ js/angular-config.js
@@ -12,6 +12,15 @@
         'nvd3'
     ]);
 
+    // constant definitions
+    as.constant('CONSTANTS', (function () {
+              
+        return {
+            API_URL: 'http://gateway.marvel.com',    // api location
+            PAGE_REFRESH_TIME: 5000         // milliseconds
+        }
+    })());
+
     // route definitions
     as.config(function($routeProvider, $httpProvider) {
         
