987d9b0eae3f76aebef5647730b9e2596c9ec3d8
victormdev <victoralexandrino99@gmail.com>
2021-12-23 14:40:57 -0300

    add method and npm

--- js/controllers/app-controller.js
+++ js/controllers/app-controller.js
@@ -1,7 +1,7 @@
 (function() {
     var as = angular.module('app.controllers', ['ngRoute', 'ngResource']);
     
-    as.controller('AppCtrl', 
+    as.controller('MainCtrl', 
         ['$scope', '$rootScope', '$http', '$location', 'AppService', 'HeroService', 'ngResource', 'CONSTANTS',
             function ($scope, $rootScope, $http, $location, AppService, HeroService, resource ,  CONSTANTS) {
 
--- js/controllers/hero/hero-controller.js
+++ js/controllers/hero/hero-controller.js
@@ -1,18 +1,46 @@
-(function () {
-    var as = angular.module('app.controllers', ['ngRoute']);
+var app = angular.module('HeroMarvel', ['ngRoute']);
 
-    as.controller('HeroShowCtrl', ['$scope', '$rootScope', '$http', '$location', 'HeroService', function ($scope, $rootScope, $http, $location, HeroService) {
 
-        $scope.heroList = [];
+app.controller('HeroCtrl', function($scope, $location, $http) {
+  $scope.char={};
+  $scope.showCharInfo= false;
+  $scope.getCharacters = function(val) {
+        $scope.timeStamp=  Date.now();
+         $scope.publicKey="5234a931fdd1da574fb6133e31a6d02c";
+        baseUrl= "http://gateway.marvel.com/v1/public/characters";
+    return $http.get(baseUrl, {
+      params: {
+        nameStartsWith: val,
+        limit: 25,
+        ts: $scope.timeStamp,
+        apikey: $scope.publicKey 
+      }
+    }).then(function(response){
+      $scope.charInfoArr=response.data.data.results;
+      return response.data.data.results.map(function(item){
+        
+        return item.name;
+      });
+    });
+  };
 
-        $scope.load = function () {
+  $scope.selectCharacter=function (item){
+    angular.forEach($scope.charInfoArr, function(obj, key){
+      if(obj.name===item){
+      
+         if (obj.thumbnail){
+           $scope.char.thumb= obj.thumbnail.path+"."+obj.thumbnail.extension;
+         }else{
+           $scope.char.thumb="";
+         }
+         
+         $scope.char.name= obj.name;
+         $scope.char.desc= obj.description;
+         $scope.showCharInfo= true;
+      }
+       
+    });
+    
+  }
 
-            Heroervice.getHeroes()
-            .success(function (result, status, headers, config) {
-                $scope.heroList = result;
-            });
-        }
-
-    }]);
-
-}());
+});
