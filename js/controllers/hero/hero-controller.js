8beee1a42d20fdd0517c7bfe7ad0449f33213e1c
victormdev <victoralexandrino99@gmail.com>
2021-12-24 18:48:28 -0300

    fix bugs

--- js/controllers/hero/hero-controller.js
+++ js/controllers/hero/hero-controller.js
@@ -1,8 +1,41 @@
 (function () {
   var as = angular.module('HeroMarvel', ['ngRoute']);
 
-  as.controller('HeroCtrl', function ($scope, $location, $http) {
-    $scope.teste = "testeee";
+  as.controller('HeroCtrl', function ($scope, $location, $http, $q) {
+
+    var ts = new Date().getTime()
+    var publicKey = "5a237863b3cc2061003cbbc4fe20dc06"
+    var privateKey = "fbf255068eccea6d0ef951b9f25626b57ab2fe72"
+
+    var hash = md5(ts + privateKey + publicKey).toString();
+
+    var baseUrl = "http://gateway.marvel.com/v1/public/";
+    var complemento = "?ts=" + ts + "&apikey=" + publicKey + "&hash=" + hash;
+
+    console.log(hash);
+
+    function getCharacters() {
+      $q.when(axios.get(baseUrl + "/characters" + complemento)
+        .then(function (res) {
+          console.log(res.data.data)
+          return res.data.data;
+        })
+        .catch(function (err) {
+          console.log(err)
+        }));
+    }
+
+    getCharacters();
+
+    function getCharacter(id) {
+      $q.when(axios.get(baseUrl + "/characters/" + id + complemento)
+        .then(function (res) {
+          console.log(res.data.data)
+        })
+        .catch(function (err) {
+          console.log(err)
+        }));
+    }
 
   });
 }());
